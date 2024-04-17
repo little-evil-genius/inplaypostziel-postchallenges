30fea9c8278c1a9f4ad06706792245888fbedc97
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-04-17 15:37:06 +0200

    Create postinggoal.lang.php

---
+++ inc/languages/deutsch_du/postinggoal.lang.php
@@ -0,0 +1,111 @@
+<?php
+$l['postinggoal_index_headline'] = "Postmarathon vom <b>{1}</b> bis <b>{2}</b>";
+$l['postinggoal_index_toplist_character'] = "Topliste nach Charakteren";
+$l['postinggoal_index_toplist_player'] = "Topliste nach Spieler*innen";
+$l['postinggoal_index_bit_rangebar_posts'] = "{1} von {2} Inplayposts";
+$l['postinggoal_index_bit_goal_posts'] = "von {1} Inplayposts";
+$l['postinggoal_index_bit_count_posts'] = "Inplayposts";
+$l['postinggoal_index_bit_rangebar_words'] = "{1} von {2} Wörtern";
+$l['postinggoal_index_bit_goal_words'] = "von {1} Wörtern";
+$l['postinggoal_index_bit_count_words'] = "Wörter";
+$l['postinggoal_index_bit_rangebar_char'] = "{1} von {2} Zeichen";
+$l['postinggoal_index_bit_goal_char'] = "von {1} Zeichen";
+$l['postinggoal_index_bit_count_char'] = "Zeichen";
+
+$l['postinggoal_redirect_toplist'] = "Die Topliste für den Postmarathon ist aktuell nicht aktiv. Du wirst nun auf den Index zurückgeleitet.";
+$l['postinggoal_toplist_nav'] = "Toplisten";
+$l['postinggoal_toplist_nav_character'] = "Charakter-Topliste";
+$l['postinggoal_toplist_nav_player'] = "Spieler*in-Topliste";
+$l['postinggoal_toplist_character'] = "Topliste der Charakteren";
+$l['postinggoal_toplist_player'] = "Topliste der Spieler*innen";
+$l['postinggoal_toplist_character_head'] = "Charakter";
+$l['postinggoal_toplist_player_head'] = "Spieler*in";
+$l['postinggoal_toplist_head_posts'] = "Posts";
+$l['postinggoal_toplist_head_words'] = "Wörter";
+$l['postinggoal_toplist_head_char'] = "Zeichen";
+
+$l['postinggoal_redirect_usercp'] = "Das Team hat die Möglichkeit für persönliche Post-Challenges deaktiviert. Du wirst nun zur Startseite vom Benutzer Control-Panel zurückgeleitet.";
+$l['postinggoal_usercp_nav'] = "persönliche Post-Challenge";
+$l['postinggoal_usercp_desc'] = "Willkommen zu deiner persönlichen Post-Challenge! Bei dieser Challenge kannst du dir selbst ein Ziel setzen und deine eigenen Fortschritte verfolgen. Deine Aufgabe ist es, eine bestimmte Anzahl von Posts innerhalb einer festgelegten Anzahl von Tagen zu erreichen. Du kannst die Anzahl der Posts und Tage ganz nach deinen Wünschen wählen, um deine persönliche Herausforderung zu gestalten. Nach Belieben kannst du dir auch Ziele für die Wörter- und Zeichenanzahl setzen.<br>Fordere dich selbst heraus und sieh, wie weit du kommen kannst! Viel Spaß beim Schreiben und Erreichen deiner Ziele!";
+
+$l['postinggoal_usercp_challenge_active_headline'] = "Aktuelle Challenge";
+$l['postinggoal_usercp_challenge_active_none'] = "<center>Aktuell hast du dir keine persönliche Post-Challenge gesetzt.</center>";
+$l['postinggoal_usercp_challenge_active_goal_full'] = "Ziel » <b>{1}</b> Posts mit <b>{2}</b> Wörtern und <b>{3}</b> Zeichen in <b>{4}</b> Tagen ({5} - {6})";
+$l['postinggoal_usercp_challenge_active_goal_words'] = "Ziel » <b>{1}</b> Posts mit <b>{2}</b> Wörtern in <b>{3}</b> Tagen ({4} - {5})";
+$l['postinggoal_usercp_challenge_active_goal_characters'] = "Ziel » <b>{1}</b> Posts mit <b>{2}</b> Zeichen in <b>{3}</b> Tagen ({4} - {5})";
+$l['postinggoal_usercp_challenge_active_goal_posts'] = "Ziel » <b>{1}</b> Posts in <b>{2}</b> Tagen ({3} - {4})";
+$l['postinggoal_usercp_challenge_active_count_posts_goal'] = "von {1} Inplayposts";
+$l['postinggoal_usercp_challenge_active_count_words_goal'] = "von {1} Wörtern";
+$l['postinggoal_usercp_challenge_active_count_words'] = "Wörtern";
+$l['postinggoal_usercp_challenge_active_count_characters_goal'] = "von {1} Zeichen";
+$l['postinggoal_usercp_challenge_active_count_characters'] = "Zeichen";
+$l['postinggoal_usercp_challenge_active_goalstatus_reached'] = "<span class=\"postinggoal_goalstatus_reached\">Das Ziel wurde erreicht!</span><br>Es bleiben noch <b>{1}</b> Tage, um das Ergebnis weiter auszubauen.";
+$l['postinggoal_usercp_challenge_active_goalstatus_notreached'] = "<span class=\"postinggoal_goalstatus_notreached\">Das Ziel wurde noch nicht erreicht!</span><br>Es bleiben noch <b>{1}</b> Tage, um das Ziel zu erreichen.";
+$l['postinggoal_usercp_challenge_characterstat_name'] = "Charakter";
+$l['postinggoal_usercp_challenge_characterstat_post'] = "Anzahl der Posts";
+$l['postinggoal_usercp_challenge_characterstat_word'] = "Anzahl der Wörter";
+$l['postinggoal_usercp_challenge_characterstat_character'] = "Anzahl der Zeichen";
+
+$l['postinggoal_redirect_add'] = "Du hast eine neue persönliche Post-Challenge gestartet. Viel Erfolg dabei! Du wirst nun auf die Seite mit den persönlichen Post-Challenges zurückgeleitet.";
+$l['postinggoal_usercp_challenge_add_headline'] = "neue Post-Challenge starten";
+$l['postinggoal_usercp_challenge_add_posts'] = "Postingziel";
+$l['postinggoal_usercp_challenge_add_words'] = "Wortziel";
+$l['postinggoal_usercp_challenge_add_characters'] = "Zeichenziel";
+$l['postinggoal_usercp_challenge_add_days'] = "Tagesanzahl";
+$l['postinggoal_usercp_challenge_add_button'] = "Challenge starten";
+
+$l['postinggoal_redirect_report'] = "Herzlichen Glückwunsch, du hast deine persönliche Post-Challenge geschafft. Damit alle es mitbekommen wird ein automatischer Post in dem entsprechendem Thema gepostet. Du wirst nun zu diesem Thema und Post weitergeleitet.";
+$l['postinggoal_usercp_challenge_active_report_button'] = "Challenge beenden?";
+$l['postinggoal_usercp_challenge_active_report_button_onClick'] = "Wenn du die Challenge jetzt schon für beendet erklärst, kannst du keine weiteren Post, Zeichen und Wörter für dieses Ergebnis sammeln? Möchtest du die Challenge jetzt schon beenden?";
+$l['postinggoal_usercp_challenge_finished_report_button'] = "Challenge melden";
+
+$l['postinggoal_usercp_challenge_report_message'] = "Hey,<br>Ich habe meine persönliche Post-Challenge erfolgreich abgeschlossen. {7}<br>Mein Ziel waren {1} Posts in {2} Tagen. {3}Geschafft habe ich {4} Posts mit {5} Wörtern und {6} Zeichen.";
+$l['postinggoal_usercp_challenge_report_message_prematuredays_plural'] = "Ich habe es sogar {1} Tage früher geschafft als geplant.";
+$l['postinggoal_usercp_challenge_report_message_prematuredays_singular'] = "Ich habe es sogar einen Tag früher geschafft als geplant.";
+$l['postinggoal_usercp_challenge_report_message_additive_full'] = "Zusätzlich habe ich mir das Ziel gesetzt {1} Wörter und {2} Zeichen und zu schreiben.<br>";
+$l['postinggoal_usercp_challenge_report_message_additive_words'] = "Zusätzlich habe ich mir das Ziel gesetzt {1} Wörter zu schreiben.<br>";
+$l['postinggoal_usercp_challenge_report_message_additive_characters'] = "Zusätzlich habe ich mir das Ziel gesetzt {1} Zeichen und zu schreiben.<br>";;
+
+$l['postinggoal_usercp_challenge_finished_headline'] = "vergangene Challenges";
+$l['postinggoal_usercp_challenge_finished_none'] = "<center>Aktuell hast du noch keine persönliche Post-Challenge beendet.</center>";
+$l['postinggoal_usercp_challenge_finished_headline_goal'] = "Ziel";
+$l['postinggoal_usercp_challenge_finished_headline_result'] = "Ergebnis";
+$l['postinggoal_usercp_challenge_finished_goal_full'] = "<b>{1}</b> Posts mit <b>{2}</b> Wörtern und <b>{3}</b> Zeichen in <b>{4}</b> Tagen<br><span class=\"postinggoal_smalltext\">({5} - {6}{7})</span>";
+$l['postinggoal_usercp_challenge_finished_goal_words'] = "<b>{1}</b> Posts mit <b>{2}</b> Wörtern in <b>{3}</b> Tagen<br><span class=\"postinggoal_smalltext\">({4} - {5}{6})</span>";
+$l['postinggoal_usercp_challenge_finished_goal_characters'] = "<b>{1}</b> Posts mit <b>{2}</b> Zeichen in <b>{3}</b> Tagen<br><span class=\"postinggoal_smalltext\">({4} - {5}{6})</span>";
+$l['postinggoal_usercp_challenge_finished_goal_posts'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen<br><span class=\"postinggoal_smalltext\">({3} - {4}{5})</span>";
+$l['postinggoal_usercp_challenge_finished_result'] = "<span class=\"postinggoal_smalltext\">{1} Posts mit {2} Zeichen und {3} Wörten.</span>";
+$l['postinggoal_usercp_challenge_finished_goalstatus_reached'] = "<span class=\"postinggoal_goalstatus_reached\">Das Ziel wurde erreicht!</span>";
+$l['postinggoal_usercp_challenge_finished_goalstatus_notreached'] = "<span class=\"postinggoal_goalstatus_notreached\">Das Ziel wurde nicht erreicht!</span>";
+$l['postinggoal_usercp_challenge_finished_prematuredays'] = " - {1} {2} früher beendet ({3})";
+
+$l['postinggoal_redirect_overview'] = "Das Team hat die Möglichkeit eine Übersicht für persönliche Post-Challenges deaktiviert. Du wirst nun auf den Index zurückgeleitet.";
+$l['postinggoal_overview_permissions'] = "Du hast keine Berechtigung die Übersicht alle persönlichen Post-Challenges einzusehen.";
+$l['postinggoal_overview_nav'] = "Übersicht der persönlichen Post-Challenges";
+$l['postinggoal_overview_nav_active'] = "Aktive Post-Challenges";
+$l['postinggoal_overview_nav_finished'] = "Vergangene Post-Challenges";
+$l['postinggoal_overview_head_player'] = "Spieler*in";
+$l['postinggoal_overview_head_goal'] = "Ziel";
+$l['postinggoal_overview_head_progress'] = "Fortschritt";
+$l['postinggoal_overview_head_result'] = "Ergebnis";
+$l['postinggoal_overview_result'] = "<span class=\"postinggoal_smalltext\">{1} Posts mit {2} Zeichen und {3} Wörten.</span>";
+$l['postinggoal_overview_reached'] = "<span class=\"postinggoal_goalstatus_reached\">Das Ziel wurde erreicht!</span>";
+$l['postinggoal_overview_notreached_active'] = "<span class=\"postinggoal_goalstatus_notreached\">Das Ziel wurde noch nicht erreicht!</span>";
+$l['postinggoal_overview_notreached_finished'] = "<span class=\"postinggoal_goalstatus_notreached\">Das Ziel wurde nicht erreicht!</span>";
+$l['postinggoal_overview_challenge_active_none'] = "<center>Aktuell hat sich kein User eine persönliche Post-Challenge gesetzt.</center>";
+$l['postinggoal_overview_challenge_finished_none'] = "<center>Bisher hat noch kein User eine persönliche Post-Challenge beendet.</center>";
+$l['postinggoal_overview_active_goal_full'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen <span class=\"postinggoal_smalltext\">(noch {3} Tage)</span><br>Zusatz: <b>{4}</b> Wörtern und <b>{5}</b> Zeichen";
+$l['postinggoal_overview_active_goal_words'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen <span class=\"postinggoal_smalltext\">(noch {3} Tage)</span><br>Zusatz: <b>{4}</b> Wörtern";
+$l['postinggoal_overview_active_goal_characters'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen <span class=\"postinggoal_smalltext\">(noch {3} Tage)</span><br>Zusatz: <b>{4}</b> Zeichen";
+$l['postinggoal_overview_active_goal_posts'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen <span class=\"postinggoal_smalltext\">(noch {3} Tage)</span>";
+$l['postinggoal_overview_finished_goal_full'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen<br>Zusatz: <b>{3}</b> Wörtern und <b>{4}</b> Zeichen<br><span class=\"postinggoal_smalltext\">({5} - {6}{7})</span>";
+$l['postinggoal_overview_finished_goal_words'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen<br>Zusatz: <b>{3}</b> Wörtern<br><span class=\"postinggoal_smalltext\">({4} - {5}{6})</span>";
+$l['postinggoal_overview_finished_goal_characters'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen<br>Zusatz: <b>{3}</b> Zeichen<br><span class=\"postinggoal_smalltext\">({4} - {5}{6})</span>";
+$l['postinggoal_overview_finished_goal_posts'] = "<b>{1}</b> Posts in <b>{2}</b> Tagen<br><span class=\"postinggoal_smalltext\">({3} - {4}{5})</span>";
+$l['postinggoal_overview_finished_prematuredays'] = " - {1} {2} früher beendet";
+
+$l['postgoals_online_location_toplist_character'] = "Schaut sich die <a href=\"misc.php?action=postgoals_character\">Topliste nach Charakteren</a> vom Postmarathon an.";
+$l['postgoals_online_location_toplist_player'] = "Schaut sich die <a href=\"misc.php?action=postgoals_character\">Topliste nach Spieler*innen</a> vom Postmarathon an.";
+$l['postgoals_online_location_overview_active'] = "Schaut sich die <a href=\"misc.php?action=postchallenges_overview_active\">aktiven persönlichen Post-Challenges</a> vom Forum an.";
+$l['postgoals_online_location_overview_finished'] = "Schaut sich die <a href=\"misc.php?action=postchallenges_overview_finished\">vergangenen persönlichen Post-Challenges</a> vom Forum an.";
+$l['postgoals_online_location_usercp'] = "Schaut sich die <a href=\"misc.php?action=usercp.php?action=postchallenges\">eigenen persönlichen Post-Challenges</a> im Usercp an.";
