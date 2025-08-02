a882ec78505b6b89712ee6c3b1bed1b09bcdb6dc
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-08-02 14:50:22 +0200

    Update postinggoal.php

--- inc/plugins/postinggoal.php
+++ inc/plugins/postinggoal.php
@@ -288,8 +288,8 @@ function postinggoal_admin_update_plugin(&$table) {
                 $table_status = $db->fetch_array($query);
                 $actual_collation = strtolower($table_status['Collation'] ?? '');
 
-                $actual_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $actual_collation);
-                $expected_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $collation);
+                $actual_collation = str_replace('utf8mb3', 'utf8', $actual_collation);
+                $expected_collation = str_replace('utf8mb3', 'utf8', $collation);
 
                 if (!empty($collation) && $actual_collation !== $expected_collation) {
                     $db->query("ALTER TABLE {$table} CONVERT TO CHARACTER SET {$charset} COLLATE {$collation}");
@@ -3208,8 +3208,8 @@ function postinggoal_is_updated(){
         $result = $db->fetch_array($query);
         $actual_collation = strtolower($result['TABLE_COLLATION'] ?? '');
         
-        $actual_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $actual_collation);
-        $expected_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $collation);
+        $actual_collation = str_replace('utf8mb3', 'utf8', $actual_collation);
+        $expected_collation = str_replace('utf8mb3', 'utf8', $collation);
 
         if ($actual_collation !== $expected_collation) {
             return false;
